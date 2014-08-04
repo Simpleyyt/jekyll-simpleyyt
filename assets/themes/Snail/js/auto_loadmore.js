1b0bede55f157faa53aae35af978ece844bff24a
Simpleyyt <simpleyyt@gmail.com>
2014-08-04 14:41:43 +0800

    Add pagination

---
+++ assets/themes/Snail/js/auto_loadmore.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    var loadbutton = $('#journal-loadmore');
+    var loadtext = loadbutton.text();
+	var url = $('.journal-arrow.ja-forward').attr("href");
+    $('#journal-arrows-cont').hide();
+    HideIfNoMorePosts();
+    function HideIfNoMorePosts() {
+        if (url == undefined || url.length == 0) {
+            loadbutton.remove();
+			$('footer').show();
+        } else {
+            loadbutton.show();
+			$('footer').hide();
+        }
+    }
+    loadbutton.click(function() {
+        moreposts();
+    });
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= ($(document).height() - ($(window).height()))) {
+            if (loadbutton.closest('body').length > 0) moreposts();
+        }
+    });
+    function moreposts() {
+        loadbutton.empty().append('Loading...');
+		var next_url = url;
+		$.get(next_url, function(data,status){
+			var articles = $(data).find('#journal-articles-block').children();
+            $('<span></span>').appendTo('#journal-articles-block').hide().append(articles).fadeIn(500);
+			url = $(data).find('.journal-arrow.ja-forward').attr("href");
+            loadbutton.empty().append(loadtext);
+			HideIfNoMorePosts();
+		});
+        HideIfNoMorePosts();
+    }
+});
---
+++ assets/themes/Snail/js/loadmore.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var loadbutton = $('#journal-loadmore');
+    var loadtext = loadbutton.text();
+	var url = $('.journal-arrow.ja-forward').attr("href");
+    $('#journal-arrows-cont').hide();
+    HideIfNoMorePosts();
+    function HideIfNoMorePosts() {
+        if (url == undefined || url.length == 0) {
+            loadbutton.remove();
+        } else {
+            loadbutton.show();
+        }
+    }
+    loadbutton.click(function() {
+        moreposts();
+    });
+    function moreposts() {
+        loadbutton.empty().append('Loading...');
+		var next_url = url;
+		$.get(next_url, function(data,status){
+			var articles = $(data).find('#journal-articles-block').children();
+            $('<span></span>').appendTo('#journal-articles-block').hide().append(articles).fadeIn(500);
+			url = $(data).find('.journal-arrow.ja-forward').attr("href");
+            loadbutton.empty().append(loadtext);
+			HideIfNoMorePosts();
+		});
+        HideIfNoMorePosts();
+    }
+});
